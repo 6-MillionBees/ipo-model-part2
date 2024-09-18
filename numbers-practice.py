7c865c9df97d4670908e18e2111dd6e2b6702929
6MillionBees <BoettcherAr321@northwested.org>
2024-09-18 11:10:36 -0400

    Add files via upload

---
+++ numbers-practice.py
@@ -0,0 +1,31 @@
+# Arden
+# 9/18/24
+# Numbers Practice
+
+from colorama import Fore # Everything that starts with Fore is for coloring the text
+
+num1 = float(input('Enter a number: '))
+operation = input(f"Enter your Operation \n{Fore.RED}ONLY{Fore.RESET} type '+' for addition, '-' for subtraction, '*' for multiplication, and '/' for division. \n")
+num2 = float(input('Enter another number: '))
+
+while True: # the while loop is for 
+    if operation == '+':
+        total = num1 + num2
+        break
+    elif operation == '-':
+        total = num1 - num2
+        break
+    elif operation == '*':
+        total = num1 * num2
+        break
+    elif operation == '/':
+        total = num1 / num2
+        break
+    else:
+        print(Fore.RED + '\nInvalid number please try again. \n' + Fore.RESET)
+        operation = input(Fore.GREEN + "Renter your Operation \nType '+' for addition, '-' for subtraction, '*' for multiplication, and '/' for division. No spaces of \n" + Fore.RESET)
+
+total_find = str(total).find('.')
+offset = int(total_find) + 3
+
+print('Your total is ' + Fore.GREEN + str(total)[0:offset] + Fore.RESET)
