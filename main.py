10b5779fd4e10e43e5027619e78927c912c6a92e
6MillionBees <BoettcherAr321@northwested.org>
2024-09-18 10:36:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 # 9/18/24
 # IPO part 2 / Python Simple-Calc
 
+# Input
 num1 = float(input('Enter a number: '))
 num2 = float(input('Enter another number: '))
 
+# Processing
 total = num1 + num2
 
 total_find = str(total).find('.') # This is my overly complex way to limit the number of decimal places to keep the answer from going off the rails
 offset = int(total_find) + 3
 
-
+# Output
 print('The sum of the two numbers is ' + str(total)[0:offset])
+
+#IPO Complete!
